41e3c8c91bd1fe2d84e801599b6aa58f39e792bc
Adam Leayr <adam@leayr.com>
2016-12-01 15:46:46 +1100

    update landing page bean

--- templates/bean--image-and-text--landing-page.tpl.php
+++ templates/bean--image-and-text--landing-page.tpl.php
@@ -45,11 +45,10 @@
  */
 ?>
 <div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-  <?php print_r($content); ?>
   <div class="content"<?php print $content_attributes; ?>>
     <div class="image-title">
       <?php if (!empty($content['field_bean_image'])) : ?>
-        <div class="image landing-test">
+        <div class="image landing-hover-<?php echo (empty($content['field_bean_imagehover'])?'no':'yes')">
         <?php if (!empty($content['field_link_to'])) : ?>
           <a href="<?php print render($content['field_link_to'][0]); ?>"><?php print render($content['field_bean_image']); ?></a>
           <?php if (!empty($content['field_bean_imagehover'])) : ?>
